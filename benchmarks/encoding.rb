4dfa6744c3b08f5cee6a4ba1f0bfbe762ff6ffe5
David Larrabee <david.larrabee@meyouhealth.com>
2013-03-26 20:25:09 -0400

    Benchmarks to test regex usage

--- benchmarks/encoding.rb
+++ benchmarks/encoding.rb
@@ -8,6 +8,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/gsm_encoder')
 
 positive_encode = " !\"#¤%&'()*+,-./0123456789:;<=>?¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ§¿abcdefghijklmnopqrstuvwxyzäöñüà^{}\[~]|€"
 negative_encode = positive_encode + 'привет'
+common_message = "this is a common phrase using just letters and numbers like 57 and 38"
 n = 5_000
 
 Benchmark.bmbm do |x|
@@ -16,4 +17,7 @@ Benchmark.bmbm do |x|
 
   x.report("negative encode:") { n.times  { GSMEncoder.encode(negative_encode) } }
   x.report("negative can_encode?:") { n.times  { GSMEncoder.can_encode?(negative_encode) } }
+
+  x.report("common encode:") { n.times { GSMEncoder.encode(common_message) } }
+  x.report("common can_encode?:") { n.times { GSMEncoder.can_encode?(common_message) } }
 end
--- benchmarks/string_vs_bytes.rb
+++ benchmarks/string_vs_bytes.rb
@@ -38,23 +38,32 @@ def string_can_encode(str)
   true
 end
 
+def regex_can_encode(str)
+  str =~ /^[A-Za-z0-9]+$/
+end
+
 Benchmark.bmbm do |r|
   N = 200_000
   CHARS_ARRAY = (' '..'_').to_a
   STRING = CHARS_ARRAY.to_a.join('')
   POSITIVE = STRING
   NEGATIVE = POSITIVE + 'привет'
+  COMMON = "this is a common phrase using just letters and numbers like 57 and 38"
 
   r.report("string positive") do
     N.times { string_can_encode(POSITIVE)}
   end
 
-  r.report("byte   positive") do    
+  r.report("byte   positive") do
     N.times { byte_can_encode(POSITIVE) }
   end
 
   r.report("ord    positive") do
     N.times { ord_can_encode(POSITIVE) }
+  end
+
+  r.report("regex  positive") do
+    N.times { regex_can_encode(POSITIVE) }
   end
 
   r.report("string negative") do
@@ -65,19 +74,40 @@ Benchmark.bmbm do |r|
     N.times { ord_can_encode(NEGATIVE) }
   end
 
-  r.report("string include?") do    
+  r.report("regex  negative") do
+    N.times { regex_can_encode(NEGATIVE) }
+  end
+
+  r.report("string include?") do
     N.times { STRING.include?('A') }
   end
 
-  r.report("string index   ") do    
+  r.report("string index   ") do
     N.times { STRING.index('A') }
   end
 
-  r.report("array  include?") do    
+  r.report("array  include?") do
     N.times { CHARS_ARRAY.include?('A') }
   end
 
-  r.report("array  index   ") do    
+  r.report("array  index   ") do
     N.times { CHARS_ARRAY.index('A') }
   end
+
+  r.report("string common phrase") do
+    N.times { string_can_encode(COMMON)}
+  end
+
+  r.report("ord    common phrase") do
+    N.times { ord_can_encode(COMMON)}
+  end
+
+  r.report("byte   common phrase") do
+    N.times { byte_can_encode(COMMON)}
+  end
+
+  r.report("regex  common phrase") do
+    N.times { regex_can_encode(COMMON)}
+  end
+
 end
